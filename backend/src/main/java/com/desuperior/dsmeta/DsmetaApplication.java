2275df32365a257ec237bbbdd8a2eff6417f9f00
Victor Soares <victor9soares@gmail.com>
2022-09-09 00:43:22 -0300

    Project created

---
+++ backend/src/main/java/com/desuperior/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.desuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
